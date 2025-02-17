f684ad5d2839faf0d3f1b8458e731ba1b9ba25fa
Daniel <danielcoblentz916@gmail.com>
2025-02-17 12:23:41 -0500

    added code for binary search, merge sort, insertion sort and quick sort

---
+++ searching/binarySearch.py
@@ -0,0 +1,18 @@
+# implementation of bineary search
+
+def binarySearch(arr: list[int], target: int) -> int:
+    L, R = 0, len(arr) - 1
+
+    while L <= R:
+        mid = (L + R) // 2
+
+        if target > arr[mid]:
+            L = mid + 1
+        elif target < arr[mid]:
+            R = mid - 1
+        else:
+            return mid
+    return -1
+
+if __name__ == "__main__":
+    print(binarySearch([1, 3, 3, 4, 5, 6, 7, 8], 8 )) # returns the index position of target
---
+++ sorting/mergeSort.py
@@ -0,0 +1,18 @@
+# Implementation of MergeSort
+def mergeSort(arr, s, e):
+    if e - s + 1 <= 1:
+        return arr
+
+    # The middle index of the array
+    m = (s + e) // 2
+
+    # Sort the left half
+    mergeSort(arr, s, m)
+
+    # Sort the right half
+    mergeSort(arr, m + 1, e)
+
+    # Merge sorted halfs
+    merge(arr, s, m, e)
+    
+    return arr
